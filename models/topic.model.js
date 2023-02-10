9ee24b0f8204f35416850c36998673d548169352
ekta <ektachandegara8888@gmail.com>
2023-02-10 16:15:49 +0530

    topic model is updated

--- models/topic.model.js
+++ models/topic.model.js
@@ -6,6 +6,11 @@ const topicSchema = new mongoose.Schema({
         unique:true,
         trim:true,
         lowercase:true
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'user',
+        required:true
     }},{timestamps:true})
 
     module.exports = mongoose.model("topic",topicSchema)
