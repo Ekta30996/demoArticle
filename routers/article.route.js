30571b464d378f265d76523d191edf931e9bd032
ekta <ektachandegara8888@gmail.com>
2023-02-10 18:52:43 +0530

    article router is updated

--- routers/article.route.js
+++ routers/article.route.js
@@ -1,4 +1,4 @@
-const  {createArticle, allArticle, articleByUser, updateArticle, deleteArticle,recentArticle} = require('../controllers/article.controller')
+const  {createArticle, allArticle, articleByUser, updateArticle, deleteArticle,recentArticle,articleByTopic} = require('../controllers/article.controller')
 const express = require('express')
 const route = express.Router()
 const auth = require('../middleware/auth')
@@ -15,5 +15,7 @@ route.get('/recent',auth,recentArticle)
 route.patch('/:id',auth, updateArticle)
 
 route.delete('/:id',auth, deleteArticle)
+
+route.get('/:id',auth,articleByTopic)
 
 module.exports = route
