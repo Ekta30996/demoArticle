7cb8e9c85cebc06d461516bd876143b240cf0674
ekta <ektachandegara8888@gmail.com>
2023-02-14 13:30:42 +0530

    all routers files changes are commited

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,9 +1,15 @@
-const {signUp,signIn} = require('../controllers/user.controller')
+const {signUp,signIn,createFollower,createUnfollow,articleOfFollowing} = require('../controllers/user.controller')
 const express = require('express')
 const route = express.Router()
+const auth = require('../middleware/auth')
 route.post('/signup',signUp)
 
 route.post('/signin',signIn)
 
+route.patch('/follow',auth,createFollower)
+
+route.patch('/unfollow',auth,createUnfollow)
+
+route.get('/articles',auth,articleOfFollowing)
 
 module.exports = route
